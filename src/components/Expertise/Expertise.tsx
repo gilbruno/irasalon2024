0b6b1a8bf7fcdb54ee08c0ddea3f4a4058f04d6d
gilbruno <gilles_bruno@gmx.fr>
2024-10-18 16:26:49 +0200

    expertise component

--- src/components/Expertise/Expertise.tsx
+++ src/components/Expertise/Expertise.tsx
@@ -5,32 +5,37 @@ import styles from './Expertise.module.scss';
 const Expertise = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image
-          src="/img/expertise/c_meulemans.jpg"
-          alt="Catherine Meulemans"
-          width={300}
-          height={200}
-          objectFit="cover"
-        />
-      </div>
-      <div className={styles.content}>
-        <h2 className={styles.title}>Le mot de notre experte</h2>
-        <div className={styles.textExpertise}>
-          Chez InRealArt, chaque œuvre est soigneusement choisie pour son caractère unique et sa capacité à sublimer votre collection. En faisant confiance à notre expertise, Catherine Sénéchal a pris une décision inspirée, tout comme tant d'autres amateurs d'art éclairés. Investir dans l'art, c'est bien plus qu'une acquisition matérielle : c'est une expérience émotionnelle, une immersion dans des histoires captivantes et des découvertes qui vibrent en harmonie avec votre sensibilité. Ajoutez une dimension nouvelle à votre collection et faites de chaque pièce une source d'inspiration personnelle.
+      <img className={styles.expert} src="/img/expertise/c_meulemans.jpg" />
+      <div className={styles.containerExpert}>
+        <div className={styles.expertTitleWords}>
+          <div className={styles.expertTitle}>Le mot de notre experte</div>
+          <div
+            className={styles.expertWords}>
+            Chez InRealArt, chaque œuvre est soigneusement choisie pour son
+            caractère unique et sa capacité à sublimer votre collection. En faisant
+            confiance à notre expertise, Catherine Sénéchal a pris une décision
+            inspirée, tout comme tant d&#039;autres amateurs d&#039;art éclairés.
+            Investir dans l&#039;art, c&#039;est bien plus qu&#039;une acquisition
+            matérielle : c&#039;est une expérience émotionnelle, une immersion dans
+            des histoires captivantes et des découvertes qui vibrent en harmonie
+            avec votre sensibilité. Ajoutez une dimension nouvelle à votre
+            collection et faites de chaque pièce une source d&#039;inspiration
+            personnelle.
+          </div>
         </div>
-        <p className={styles.signature}>
-          Meulemans Catherine<br />
-          Head of Sales<br />
-          Fondatrice Galerie Art Thema
-        </p>
-        <div className={styles.navigation}>
-          <button className={styles.navButton}>&larr;</button>
-          <button className={styles.navButton}>&rarr;</button>
+        <div className={styles.identityExpertContainer}>
+          <div className={styles.identityExpert}>
+            Meulemans Catherine
+            <br />
+            Head of Sales
+            <br />
+            Fondatrice Galerie Art Thema
+          </div>
         </div>
       </div>
     </div>
-  );
+
+  )
 };
 
 export default Expertise;
