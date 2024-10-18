be93a6a2c656f03d9c2ab7811b0afb356f9a6644
gilbruno <gilles_bruno@gmx.fr>
2024-10-18 17:45:53 +0200

    Add supabase sdk

---
+++ src/utils/supabase/supabaseFunctions.ts
@@ -0,0 +1,18 @@
+import { CODE_UNIQUE_KEY_VIOLATION } from "./constants"
+import { supabase } from "./supabaseConnection"
+
+//------------------------------------------------------------------------------ insertEmail
+export const insertDataSalonAutomne2024 = async (table: string, email: string, resourcePath: string) => {
+    let msgError = ''
+    const { error } = await supabase
+        .from(table)
+        .insert({ email: email, resourcePath: resourcePath })
+    if (error?.code == CODE_UNIQUE_KEY_VIOLATION) {
+        msgError = 'This email already exists in our e-mail base'    
+    }
+    else {
+        if (error) throw error  
+    }
+    return msgError
+}
+  
