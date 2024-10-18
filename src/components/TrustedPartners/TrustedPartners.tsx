b33e1ce1a656511c1d326c1c91cbca4c91328d0d
gilbruno <gilles_bruno@gmx.fr>
2024-10-18 14:08:20 +0200

    Partners component

--- src/components/TrustedPartners/TrustedPartners.tsx
+++ src/components/TrustedPartners/TrustedPartners.tsx
@@ -5,26 +5,14 @@ import styles from './TrustedPartners.module.scss';
 const TrustedPartners = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Ils nous font confiance</h2>
-      <div className={styles.partnersContainer}>
-        <div className={styles.partner}>
-          <Image
-            src="/img/partners/leadouze.jpg"
-            alt="Galerie A. LEADOUZE"
-            width={200}
-            height={100}
-            objectFit="contain"
-          />
+      <div className={styles.wrapperText}>
+        <div className={styles.titleContainer}>
+          <div className={styles.title}>Ils nous font confiance</div>
         </div>
-        <div className={styles.partner}>
-          <Image
-            src="/img/partners/art_thema.jpg"
-            alt="Art Thema"
-            width={150}
-            height={150}
-            objectFit="contain"
-          />
-        </div>
+      </div>
+      <div className={styles.partnersLogo}>
+        <Image className={styles.partner1} src="/img/partners/leadouze.jpg" alt="Galerie A. LEADOUZE" width={200} height={100}/>
+        <Image className={styles.partner2} src="/img/partners/art_thema.jpg" alt="Art Thema"  width={150} height={150}/>
       </div>
     </div>
   );
