b99458080cf18d26f852a9dd4499170f76167c17
gilbruno <gilles_bruno@gmx.fr>
2024-10-18 15:43:14 +0200

    Change some css

--- src/components/EventInfo/EventInfo.tsx
+++ src/components/EventInfo/EventInfo.tsx
@@ -3,10 +3,11 @@ import styles from './EventInfo.module.scss';
 export default function EventInfo() {
   return (
     <div className={styles.eventInfoContainer}>
-      <div className={styles.eventInfoItem}>
-        <h2 className={styles.title}>Venez rencontrer Catherine Sénéchal et ses oeuvres sur son emplacement</h2>
+      <div className={styles.eventInfoItem1}>
+        <h2 className={styles.title}>Catalogue InReal Art 2024 <br/>
+          Sénéchal &amp;</h2>
       </div>
-      <div className={styles.eventInfoItem}>
+      <div className={styles.eventInfoItem2}>
         <p className={styles.date}>Quand : du 23 au 27 octobre 2024</p>
       </div>
       <div className={styles.button}>
