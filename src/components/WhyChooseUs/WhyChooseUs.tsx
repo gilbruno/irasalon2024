d07ac58853abb13dc316c06dcaf4b398ff5a53b3
gilbruno <gilles_bruno@gmx.fr>
2024-10-18 13:32:17 +0200

    whychooseus component

--- src/components/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/WhyChooseUs/WhyChooseUs.tsx
@@ -15,49 +15,67 @@ const WhyChooseUs = () => {
       <div className={styles.cardsContainer}>
         <div className={styles.card}>
           <div className={styles.imageContainer}>
-            <Image src="/img/whyChooseUs/whyChooseUs.jpg" alt="Why Choose Us" width={20} height={20} />
+            <Image src="/img/whyChooseUs/whyChooseUs.jpg" alt="Why Choose Us" width={18} height={18} />
           </div>
 
-          <h3 className={styles.cardTitle}>Very Rare</h3>
-          <p className={styles.cardText}>
-            Forte de plus de 80 ans d'expérience d'années cumulées, notre équipe sélectionne moins de 100 œuvres d'artistes renommés.
-          </p>
-          <p className={styles.cardText}>
-            Chaque pièce, soigneusement choisie, est sélectionnée pour son caractère unique et son potentiel à enrichir une collection.
-          </p>
-          <p className={styles.cardText}>
-            Pour les collectionneurs, qu'ils soient expérimentés ou novices, acquérir des œuvres rares est un privilège permettant de se démarquer tout en donnant une valeur significative à leur collection.
-          </p>
+          <div className={styles.cardTitle}>Very Rare</div>
+          <div className={styles.cardText}>
+            Forte de plus de 80 ans d’expérience d’années cumulées, notre équipe
+            sélectionne moins de 100 œuvres d’artistes renommés.
+            <br />
+            <br />
+            Chaque pièce, soigneusement choisie, est sélectionnée pour son
+            caractère unique et son potentiel à enrichir une collection.
+            <br />
+            <br />
+            Pour les collectionneurs, qu’ils soient expérimentés ou novices,
+            acquérir des œuvres rares est un privilège permettant de se démarquer
+            tout en donnant une valeur significative à leur collection.
+          </div>
         </div>
         <div className={styles.card}>
           <div className={styles.imageContainer}>
-            <Image src="/img/whyChooseUs/whyChooseUs.jpg" alt="Why Choose Us" width={20} height={20} />
+            <Image src="/img/whyChooseUs/whyChooseUs.jpg" alt="Why Choose Us" width={18} height={18} />
           </div>
-          <h3 className={styles.cardTitle}>Une Marketplace Unique</h3>
-          <p className={styles.cardText}>
-            Notre marketplace se distingue par sa singularité et son innovation, intégrant harmonieusement des œuvres physiques, des NFTs, et des droits d'auteur fractionnés.
-          </p>
-          <p className={styles.cardText}>
-            Grâce à la blockchain, nous garantissons la transparence et la sécurité des transactions tout en assurant la traçabilité de chaque œuvre.
-          </p>
-          <p className={styles.cardText}>
-            Cette approche novatrice permet aux collectionneurs d'accéder à des œuvres diversifiées et uniques, tout en participant à un marché artistique de pointe.
-          </p>
+          <div className={styles.cardTitle}>Une Marketplace Unique</div>
+
+          <div className={styles.cardText}>
+            Notre marketplace se distingue par sa singularité et son innovation,
+            intégrant harmonieusement des œuvres physiques, des NFTs, et des
+            droits d&#039;auteur fractionnés.
+            <br />
+            <br />
+            Grâce à la blockchain, nous garantissons la transparence et la
+            sécurité des transactions tout en assurant la traçabilité de chaque
+            œuvre.
+            <br />
+            <br />
+            Cette approche novatrice permet aux collectionneurs d&#039;accéder à
+            des œuvres diversifiées et uniques, tout en participant à un marché
+            artistique de pointe.
+          </div>  
         </div>
         <div className={styles.card}>
           <div className={styles.imageContainer}>
-            <Image src="/img/whyChooseUs/whyChooseUs.jpg" alt="Why Choose Us" width={20} height={20} />
+            <Image src="/img/whyChooseUs/whyChooseUs.jpg" alt="Why Choose Us" width={18} height={18} />
           </div>
-          <h3 className={styles.cardTitle}>Opportunité d'Investissement</h3>
-          <p className={styles.cardText}>
-            L'art n'est pas seulement une passion, c'est aussi une opportunité d'investissement. Notre plateforme vous permet de diversifier votre portefeuille en acquérant des œuvres physiques et numériques ainsi que des droits d'auteur.
-          </p>
-          <p className={styles.cardText}>
-            Grâce à la rareté des pièces proposées, chaque acquisition a le potentiel de prendre de la valeur avec le temps.
-          </p>
-          <p className={styles.cardText}>
-            Que vous soyez novice ou expert, vous rejoignez ici un marché où l'art peut devenir une source de valorisation à long terme.
-          </p>
+          <div className={styles.cardTitle}>Opportunité d'Investissement</div>
+          <div className={styles.cardText}>
+            L&#039;art n’est pas seulement une passion, c’est aussi une
+            opportunité d&#039;investissement.
+            <br />
+            Notre plateforme vous permet de diversifier votre portefeuille en
+            acquérant des œuvres physiques et numériques ainsi que des droits
+            d&#039;auteur.
+            <br />
+            <br />
+            Grâce à la rareté des pièces proposées, chaque acquisition a le
+            potentiel de prendre de la valeur avec le temps.
+            <br />
+            <br />
+            Que vous soyez novice ou expert, vous rejoignez ici un marché où
+            l&#039;art peut devenir une source de valorisation à long terme.
+          </div>
         </div>
       </div>
     </div>
